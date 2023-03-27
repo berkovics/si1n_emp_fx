ab9fd5cf1f4f34555f5a9eee1d851407ca2cc590
Berkovics Gellért <berkovicsgellert@ktch.hu>
2023-03-27 16:06:25 +0200

    Név menő már látszik

--- src/views/TablePanel.java
+++ src/views/TablePanel.java
@@ -1,11 +1,26 @@
 package views;
 
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import models.Employee;
 
 public class TablePanel extends TableView<Employee> {
+    TableColumn<Employee, String> name_col;
 
     public TablePanel() {
+        this.createColumns();
+        th.addColumns();
+    
     }
 
+
+    private void createColumns() {
+        this.name_col = new TableColumn<>("Név");
+        this.name_col.setCellValueFactory(new PropertyValueFactory<>("name"));
+    }
+
+    private void addColumns() {
+        this.getColumns().add(name_col);
+    }
 }
